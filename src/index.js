ba9bdf6a7743eedf0642fd9a6885792d698c0712
isamrish <askmaurya48@gmail.com>
2020-04-19 03:00:01 +0530

    Add js-dev-env upto 9th module - linting
    Signed-off-by: isamrish <askmaurya48@gmail.com>

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import "./index.css";
+import numeral from "numeral";
+
+const courseValue = numeral(1000).format("$0,0.00");
+console.log(`I would page ${courseValue} for this awesome course.`); // eslint-disable-line no-console
